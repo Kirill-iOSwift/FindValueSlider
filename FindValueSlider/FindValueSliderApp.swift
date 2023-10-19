b3049a5fefe9ae419f08f575400d5f55bd776685
Kirill <133357229+kirillussr@users.noreply.github.com>
2023-10-19 14:38:46 +0300

    Initial Commit

---
+++ FindValueSlider/FindValueSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindValueSliderApp.swift
+//  FindValueSlider
+//
+//  Created by Кирилл on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FindValueSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
